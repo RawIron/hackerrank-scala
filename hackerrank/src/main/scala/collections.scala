78a3c656835d65a358e276480d8a9b7c89e9c785
RawIron <manianology@gmail.com>
2017-01-09 18:09:51 +0100

    fix compile errors.

--- hackerrank/src/main/scala/collections.scala
+++ hackerrank/src/main/scala/collections.scala
@@ -1,16 +1,19 @@
+object Collect extends App {
 
-val names = List("Her", "Jar")
+  val names = List("Her", "Jar")
+  names.filter(_.startsWith("J")).foreach(println)
 
-names.filter(_.startsWith("J")).foreach(println)
 
+  /*
+   *  exists one element that is in both sets?
+   */
 
-/*
- *  exists one element that is in both sets?
- */
+  val a: Set[Int] = Set(1,3)
+  val b: Set[Int] = Set(4,5)
 
-val a: Set[Int] = Set(1,3)
-val b: Set[Int] = Set(4,5)
+  implicit def pimp(n:Int) = new { def ! = (1 /: (1 to n)) ( _ * _ ) }
 
-implicit def pimp(n:Int) = new { def ! = (1 /: (1 to n)) ( _ * _ ) }
+  println((2+4)!)
 
-println((2+4)!)
+}
+

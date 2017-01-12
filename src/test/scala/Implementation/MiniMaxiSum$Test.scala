c8e5af2b34a06adf604e25ba3c9fba2a5d19be29
RawIron <manianology@gmail.com>
2017-01-12 10:42:50 +0100

    fix test, remove copy-pasted local class definition.

--- src/test/scala/Implementation/MiniMaxiSum$Test.scala
+++ src/test/scala/Implementation/MiniMaxiSum$Test.scala
@@ -5,11 +5,9 @@ import org.scalatest.FunSuite
 
 class MiniMaxiSum$Test extends FunSuite {
 
-  class MiniMaxi$Test extends FunSuite {
     test("small numbers") {
       val numbers: Seq[Long] = Seq(2, 3, 6, 3, 2)
-      assert (MiniMaxiSum.ofFour(numbers) == (11, 11))
+      assert (MiniMaxiSum.ofFour(numbers) == (10, 14))
     }
-  }
 
 }

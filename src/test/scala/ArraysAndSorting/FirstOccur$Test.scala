079b1d90ebacb0ddad4bbdfeccd26a681ce0700a
RawIron <manianology@gmail.com>
2017-01-12 10:41:57 +0100

    fix typo in test description.

--- src/test/scala/ArraysAndSorting/FirstOccur$Test.scala
+++ src/test/scala/ArraysAndSorting/FirstOccur$Test.scala
@@ -5,13 +5,13 @@ import org.scalatest.FunSuite
 
 class FirstOccur$Test extends FunSuite {
 
-  test("fist match using recursion") {
+  test("first match using recursion") {
     val needle: Int = 12
     val haystack: Array[Int] = "2 45 23 -4 12 343".split(" ").map(_.toInt)
     assert(FirstOccur.recursive(haystack.toList, needle) == 4)
   }
 
-  test("fist match of needle in haystack using iteration") {
+  test("first match using iteration") {
     val needle: Int = 12
     val haystack: Array[Int] = "2 45 23 -4 12 343".split(" ").map(_.toInt)
     assert(FirstOccur.loop(haystack, needle) == 4)
